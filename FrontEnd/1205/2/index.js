c61a86079a2629af4caa2225f93a08b7b97f6d38
UraiZoe <urazoe366@hengersor.hu>
2024-12-06 10:53:43 +0100

    10.58

--- FrontEnd/1205/2/index.js
+++ FrontEnd/1205/2/index.js
@@ -10,6 +10,17 @@ console.log(exports);       //
 */
 
 console.log("Hello World!");
+
+
 const add = require('./calculator.js');
 console.log(add(2, 3));
+
+
 require('http');
+
+const server =http.createServer(function(req, res){
+    res.writeHead(280);
+    res.end('Hello World!');
+});
+
+server.listen(8080);
