aa9c5cf89283d9c80533daf6fb163828829d65ec
UraiZoe <urazoe366@hengersor.hu>
2024-12-06 10:25:32 +0100

    10.25

--- FrontEnd/1205/2/index.js
+++ FrontEnd/1205/2/index.js
@@ -1,7 +1,12 @@
 //wraapper function
+/*
 console.log("ok");
-console.log(_dirname);
-console.log(_filename);
-console.log(module);
-console.log(require);
-console.log(exports);
+console.log(_dirname);      //file name
+console.log(_filename);     //elérési utvonal
+//abszolut könyvtár     https://hu.wikipedia.org/wiki/El%C3%A9r%C3%A9si_%C3%BAt
+console.log(module);        
+console.log(require);       //
+console.log(exports);       //
+*/
+
+console.log("Hello World!");
