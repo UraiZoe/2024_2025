3cd806b1c9ad60a074d2038eb5ddbbc3f3563213
UraiZoe <urazoe366@hengersor.hu>
2024-12-09 12:24:47 +0100

    hibas

--- React/1209/app.js
+++ React/1209/app.js
@@ -1,1 +1,17 @@
-console.log();
+const btn = document.createElement("button");
+btn.onclick = function () {
+    alert("Natív gomb megnyomva!")
+}
+btn.innerHTML = "Natív gomb";
+document.getElementById("nativ-button-container").appendChild(btn);
+
+const gomb = React.createElement("button",      "gomb": Unkown word.
+    {
+        onclick: function()
+        {
+            alert("Ez eg react kód!");
+        },
+    },
+    "React gomb"    "gomb": Unkown word.
+)
+ReactDom.rander(gomb,documentElementById("react-button-container"));
