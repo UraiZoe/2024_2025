fd268599292f97a1a83f374af5c0a1f95b79a411
UraiZoe <urazoe366@hengersor.hu>
2025-01-27 12:03:14 +0100

    ???

--- React/kozut/backend/index.js
+++ React/kozut/backend/index.js
@@ -9,18 +9,18 @@ app.use(cors());
 const db = mysql.createConnection({
     user: "root",
     host: "127.0.0.1",
-    port: 3307,
+    port: 3306,
     password: "",
     database: "kozutak"
 });
 
 app.get("/", (req, res) => {
-    res.send("Henlo Henlo a Backend müködik!");
+    res.send("panda");
 });
  
 app.get("/regiok", (req, res) => {
     const sql = "SELECT * FROM regiok";
-    db.query(sql, id, (err, result) => {
+    db.query(sql, (err, result) => {
         if (err) return res.json(err);
         return res.json(result);
     });
@@ -35,7 +35,31 @@ app.get("/regiokwhere", (req, res) => {
         return res.json(result);
     });
 });
- 
+
+app.post("/ujregio", (req, res) => {
+    const sql = "INSERT INTO `regiok` (`Rid`, `regionev`, `regio_tipusa`) VALUES (?, ?, ?), (?, ?, ?)";
+    const values = ["10", "Budapest", "főváros", "11", "Pest", "régió"];
+   
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            //return res.json(err);
+            console.error("Hiba történt: ", err);
+            return res.status(500).json({error: "Adatbázis hiba történt!"});
+        }
+        //return res.json(result);
+        return res.status(200).json({message: "Sikeres beszúrás!", result});
+    });
+});
+
+app.delete("/regiotorlese/:id", (req, res) => {
+    const sql = "DELETE FROM `regiok` WHERE `Rid` = ?";
+    db.query(sql, (err, result) => {
+        if (err) {return res.json(err);}
+        return res.json(result);
+    });
+});
+
+
 app.listen(3001, () => {
     console.log("A szerver elindult a 3001-es porton.");
 });
