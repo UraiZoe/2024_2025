71f8536e073ecf43499e428a8e8f1820d6c21f99
UraiZoe <urazoe366@hengersor.hu>
2025-02-06 12:48:33 +0100

    0206

---
+++ React/2025-02-06/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1>Tz</h1>
+      <div>
+        <p>Ez a fő oldal.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
